fd819fefb493ee4aa519f21e920e8ee9f6f0a1a5
Miharu380 <38t.ajlkk@gmail.com>
2021-06-16 09:55:14 +0000

    deviseコントローラー削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :given_name, :kana_family_name, :kana_given_name, :postal_code, :address, :phone_number])
+  end
 end
