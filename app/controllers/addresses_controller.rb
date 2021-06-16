b5f5044f7200f05ae0579813a31afaf10f526203
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-06-16 07:23:10 +0000

    addresses,items, cart_itemsのcontrollerとviewファイル作成

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,42 @@
+class AddressesController < ApplicationController
+  def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      redirect_to addresses_path  # render?
+      flash[:alert] = ""
+    end
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update
+      redirect_to addresses_path
+    else
+      redirect_to request.referer
+    end
+  end
+
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+end
