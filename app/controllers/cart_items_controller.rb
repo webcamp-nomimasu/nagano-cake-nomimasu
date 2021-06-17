d4010cecc5b9d57e2f9adf761f2abeb27211d2e9
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-06-17 02:24:38 +0000

    cart_itemsコントローラー記述

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,21 +2,34 @@ class CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @cart_items = CartItem.all
+    @cart_items = CartItem.where(customer_id: current_customer.id)
   end
 
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
     @cart_items = current_customer.cart_items.all
-    @cart_items.each do |cart_item|
+    if cart_item.amount != nil
+      @cart_items.each do |cart_item|
+        if cart_item.item_id == @cart_item.item_id
+          add_amount = cart_item.amount + @cart_item.amount
+          cart_item.update(:amount, add_amount)
+          @cart_item.delete
+        end
+      end
+      @cart_item.save
+      flash[:success] = "カートに商品を追加しました"
+      redirect_to cart_item_path(current_customer)
+    else
+      redirect_to request.referer
+      flash[:danger] = "数量を指定してください。"
     end
-
   end
 
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_items = CartItems.where(customer_id: current_customer.id)
     @cart_item.update(cart_item_params)
+    redirect_to customer_cart_items_path
   end
 
   def destroy
@@ -35,5 +48,4 @@ class CartItemsController < ApplicationController
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :customer_id, :amount)
   end
-
 end
