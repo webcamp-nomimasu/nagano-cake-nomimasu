14cb9e1cedd9bfe75d48b2c1890b6be5923d1d5b
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-06-17 05:56:54 +0000

    developと統合

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,4 +1,4 @@
-class ItemsController < ApplicationController
+class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page]).per(8).order(:id)
     @genres = Genre.all
