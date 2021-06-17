246a4c0eda1cb5d1e64bafecb4f9d2044812d192
EC2 Default User <ec2-user@ip-172-31-7-243.ec2.internal>
2021-06-17 04:55:17 +0000

    admin側のヘッダー共通のため一時push

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,17 +1,27 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page]).reverse_order
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.find(params[:id])
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render request.referer
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
