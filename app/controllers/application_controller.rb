7b1dcba64d69e04231196aef2f93f7dcd730ff84
EC2 Default User <ec2-user@ip-172-31-7-243.ec2.internal>
2021-06-17 01:04:25 +0000

    ログイン・ログアウト機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_admin!
+  
+  
+  def after_sign_in_path_for(resource)
+    admin_orders_path
+  end 
+  
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_session_path
+  end
 end
