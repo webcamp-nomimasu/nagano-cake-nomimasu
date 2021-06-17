878fc20cb1c9c74cd6312188ce8e0809d28f383a
EC2 Default User <ec2-user@ip-172-31-7-243.ec2.internal>
2021-06-17 15:11:20 +0000

    商品関係の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_060651) do
+ActiveRecord::Schema.define(version: 2021_06_17_094902) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -47,14 +47,6 @@ ActiveRecord::Schema.define(version: 2021_06_16_060651) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "family_name", null: false
-    t.string "given_name", null: false
-    t.string "kana_family_name", null: false
-    t.string "kana_given_name", null: false
-    t.string "postal_code", null: false
-    t.string "address", null: false
-    t.string "phone_number", null: false
-    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
@@ -73,7 +65,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_060651) do
     t.text "information"
     t.integer "price"
     t.string "image_id"
-    t.boolean "is_active"
+    t.boolean "is_active", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
