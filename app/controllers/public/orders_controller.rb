4e3bdd989db079dc7a869b99011f142b6bbc1673
Miharu380 <38t.ajlkk@gmail.com>
2021-06-18 14:39:21 +0000

    confirm_page仮作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,13 +1,40 @@
 class Public::OrdersController < ApplicationController
- 
+
   def new
     @order = Order.new
     @addresses = Address.where(customer: current_customer)
   end
-  
+
   def confirm
     @cart_items = current_customer.cart_items
-    @order = Order.new(order.params)
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    # @total_price = @cart_items.sum{|c| c.item.add_tax_price * c.amount }
+    # @total_price = total_order_price
+    @sum = 0
+    @shipping_cost = 800
+    @total_price = @sum + @shipping_cost
+    # @order.total_price = @order.shipping_cost + @total_price
+    if params[:order][:address_option] == "0"
+      @order.address = current_customer.address
+      @order.postal_code = current_customer.postal_code
+      @order.name = current_customer.family_name + current_customer.given_name
+    elsif params[:order][:address_option] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.address = @address.address
+      @order.postal_code = @address.postal_code
+      @order.name = @address.name
+    elsif params[:order][:address_option] == "2"
+      @order = Order.new(order_params)
+    end
   end
-  
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :shipping_cost, :total_price, :order_status )
+  end
+
+
 end
