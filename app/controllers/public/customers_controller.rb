ad734843292e3aa2bba3bb6c38c29363f015d59a
Miharu380 <38t.ajlkk@gmail.com>
2021-06-18 02:50:55 +0000

    unsubscribe_page追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,9 +3,31 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = current_customer
   end
-  
+
   def edit
     @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customer_path
+    else
+      render "edit"
+    end
+  end
+
+  def unsubscribe
+  end
+  
+  def withdraw
+  end
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :given_name, :kana_family_name, :kana_given_name, :postal_code, :address, :phone_number, :email)
   end
 end
 
