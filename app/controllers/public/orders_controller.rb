9b8c53e42a23ebf207ad6ee6071d6c6e54cf8ab9
Miharu380 <38t.ajlkk@gmail.com>
2021-06-20 05:35:32 +0000

    order系view微調整

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,21 +4,17 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
     @addresses = Address.where(customer: current_customer)
-    # if current_customer.cart_items.empty?
-    #   redirect_to cart_items_path
-    # end
+    if current_customer.cart_items.empty?
+      redirect_to cart_items_path
+    end
   end
 
   def confirm
     @cart_items = current_customer.cart_items
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
-    # @total_price = @cart_items.sum{|c| c.item.add_tax_price * c.amount }
-    # @total_price = total_order_price
     @sum = 0
     @shipping_cost = 800
-    # @total_price = @sum + @shipping_cost
-    # @order.total_price = @order.shipping_cost + @total_price
     if params[:order][:address_option] == "0"
       @order.address = current_customer.address
       @order.postal_code = current_customer.postal_code
@@ -50,10 +46,11 @@ class Public::OrdersController < ApplicationController
       @order_item = OrderItem.new
       @order_item.item_id = cart_item.item.id
       @order_item.order_id = @order.id
-      @order_items.price = cart_item.item.price
-      @order_items.amount = cart_item.amount
+      @order_item.price = cart_item.item.price
+      @order_item.amount = cart_item.amount
+      @order_item.making_status = 0
       @order_item.save!
-      current_customer.cart_item.destroy_all
+      current_customer.cart_items.destroy_all
     end
     redirect_to orders_complete_path
   end
@@ -63,7 +60,6 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
-    @sum = 0
     @shipping_cost = 800
     @order = Order.find(params[:id])
     @order_items = @order.order_items
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,11 +5,6 @@ class Order < ApplicationRecord
   enum payment_method: {クレジットカード:0, 銀行振込:1}
   attribute :status, :integer, default: "0"
   enum status: {入金待ち:0, 入金確認:1,  製作中:2, 発送準備中:3, 発送済み:4}
-  
-  def tax_included_price
-    tax = 1.1
-    price * tax
-  end
 
   NUMBER_REGEXP = /\A[0-9]+\z/
 
