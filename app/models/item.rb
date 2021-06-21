0962337b05cfe766bdf3dce451a7c39feaadb35f
Miharu380 <38t.ajlkk@gmail.com>
2021-06-21 02:13:25 +0000

    searchコントローラー削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
   belongs_to :genre
-
+  has_many :comment
   attachment :image
 
   validates :name, presence: true
