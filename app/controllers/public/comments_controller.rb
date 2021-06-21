5354318b0889f368f07fe2346c7686a322649d37
Miharu380 <38t.ajlkk@gmail.com>
2021-06-21 04:52:55 +0000

    Ajax_comment機能完成仮

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,32 +1,28 @@
 class Public::CommentsController < ApplicationController
   before_action :authenticate_user!
-  
-  def create
-    @comment = Comment.new(commnet_params)
-    @item = @comment.item
-    if @comment.save
-      respond_to :js
-    else
-      flash[:alert] = "コメントに失敗しました"
-    end
-  end
-  
-  def destroy
-    @comment = Comment.find(params[:id])
-    @item = @comment.item
-    if @comment.destory
-      respond_to :js
-    else
-      flash[:alert] = "コメントの削除に失敗しました"
-    end
-  end
-  
+
   def index
+    @item = Item.find(params[:item_id])
+    @comment = Comment.new
   end
-  
+
+  def create
+    @item = Item.find(params[:item_id])
+    @comment = Comment.new
+    comment = @item.comments.new(comment_params)
+    comment.customer_id = current_customer.id
+    comment.save
+  end
+
+  def destroy
+    @item = Item.find(params[:item_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
+
   private
-  
+
   def comment_params
-    params.require(:comment).permit(:customer_id, :item_id, :comment)
+    params.require(:comment).permit(:comment)
   end
 end
