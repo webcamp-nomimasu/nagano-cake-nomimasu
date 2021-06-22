5d14c408a262117bd43d1d3e6cdeba33f3400dae
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-06-22 16:19:46 +0000

    admin-link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,9 +31,28 @@ $(window).on('load',function(){
 
 });
 
-$(window).on('keydown',function (e) {
-  console.log(e);
-  if (e.keyCode == 89) {
-    $('#admin-link').show();
+$(window).keydown(function (e1) {
+  console.log(e1);
+  if (e1.keyCode == 89) {
+    // 89 = y
+    $(window).keydown(function (e2) {
+    console.log(e2);
+    if (e2.keyCode == 69) {
+      // 69 = e
+      $(window).keydown(function (e3) {
+      console.log(e3);
+      if (e3.keyCode == 83) {
+        // 83 = s
+        $('#nomimasu').click(function(){
+          $('#admin-link').show();
+      })}
+    })}
+  })}
+});
+
+$(window).keydown(function (e4){
+  if(e4.keyCode == 81) {
+    // 81 = q
+    $('#admin-link').hide();
   }
 });
