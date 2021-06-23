b6b3d9041ee2b8c03ffdca5882ae4fb005c55dec
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-06-23 04:04:45 +0000

    fix pagenation

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,17 +1,15 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items_count = Item.all
     @items = Item.all.page(params[:page]).per(8)
     @genres = Genre.all
     @genre = Genre.find_by(id: params[:genre_id])
   end
 
   def search
-    # if params([:genre_id]).present?
-      @genre = Genre.find_by(id: params[:genre_id])
-    # else
-      # @genres = Genre.all
-  #   end
-      @items = Item.all
+    @genre = Genre.find_by(id: params[:genre_id])
+    @items = Item.all.page(params[:page]).per(8)
+    @items_count = Item.all
   end
 
   def show
