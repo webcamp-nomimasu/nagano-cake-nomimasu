964bc498cdea6baf8d2f88c9ad7039d141832a47
Miyu-N <myu.star1217@gmail.com>
2021-06-23 09:05:19 +0000

    カスタマー側コンタクトフォーム

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2021_06_23_043107) do
     t.string "name", null: false
     t.string "email", null: false
     t.string "phone_number", null: false
-    t.integer "subject", default: 0, null: false
+    t.text "subject", null: false
     t.text "message", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
